2e2bd7dfa9d529657c0501020a29b69044afae5e
himanshupatni <himanshu.patni12@gmail.com>
2019-05-01 20:43:35 +0530

    CRUD_Oshop

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,39 +1,51 @@
-import { Router, ActivatedRoute } from '@angular/router';
-import { Component, OnInit } from '@angular/core';
-import { ProductService } from 'src/app/product.service';
+import { Router, ActivatedRoute } from "@angular/router";
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { ProductService } from "src/app/product.service";
+import { Subscription } from "rxjs";
 
 @Component({
-  selector: 'app-admin-products',
-  templateUrl: './admin-products.component.html',
-  styleUrls: ['./admin-products.component.scss']
+  selector: "app-admin-products",
+  templateUrl: "./admin-products.component.html",
+  styleUrls: ["./admin-products.component.scss"]
 })
-export class AdminProductsComponent implements OnInit {
-product$=[];
-
-  constructor( private productService:ProductService) {
-
-    this.productService.getAll().subscribe((actions:any) => {
-
-      actions.forEach(action => {
-
-        this.product$.push({id:action.key,category:action.payload.val().category
-          ,imageUrl:action.payload.val().imageUrl,price:action.payload.val().price
-          ,title:action.payload.val().title});
-
+export class AdminProductsComponent implements OnInit, OnDestroy {
+  product$ = [];
+  subscription: Subscription;
+  fileteredProducts: any[];
+  p: Boolean = true;
+  f: Boolean = false;
+  constructor(private productService: ProductService) {
+    this.subscription = this.productService
+      .getAll()
+      .subscribe((actions: any) => {
+        this.fileteredProducts = actions.forEach(action => {
+          this.product$.push({
+            id: action.key,
+            category: action.payload.val().category,
+            imageUrl: action.payload.val().imageUrl,
+            price: action.payload.val().price,
+            title: action.payload.val().title
+          });
+        });
       });
-    });
 
-
-setTimeout(() => {
-  console.log(this.product$);
-}, 2000);
-
-
-
-   }
-
-
-  ngOnInit() {
+    setTimeout(() => {
+      console.log(this.product$);
+    }, 2000);
   }
+  ngOnInit() {}
 
+  filter(keyword) {
+    console.log(keyword);
+    this.f = true;
+    this.p = false;
+    this.fileteredProducts = keyword
+      ? this.product$.filter(p =>
+          p.title.toLowerCase().includes(keyword.toLowerCase())
+        )
+      : this.product$;
+  }
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
